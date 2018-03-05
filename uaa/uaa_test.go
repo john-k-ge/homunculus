da6c552198ce4eee06f0cc44bb1d21b25132a887
bean <bean@MacBook-Pro.local>
2018-03-04 16:06:33 -0800

    added everything

--- uaa/uaa_test.go
+++ uaa/uaa_test.go
@@ -6,24 +6,49 @@ import (
 )
 
 const (
-	uaaHost = "uaa.system.aws-usw02-pr.ice.predix.io"
-	uid     = "predix-support_systemuser"
-	pass    = ">pTz5yvM97N@y_G"
+	goodUaaHost = "uaa.system.aws-usw02-pr.ice.predix.io"
+	goodUid     = "predix-support_systemuser"
+	goodPass    = ">pTz5yvM97N@y_G"
+	badUaaHost  = "localhost"
+	badUid      = "unga"
+	badPass     = "bunga"
 )
 
-func TestNewUaaClient(t *testing.T) {
-	u := NewUaaClient(uaaHost, uid, pass)
-	if u == nil {
-		log.Printf("Failed to create UAA client with: %v, %v, %v", uaaHost, uid, pass)
+func TestUaaClient_AuthenticateGood(t *testing.T) {
+	u := NewUaaClient(goodUaaHost, goodUid, goodPass)
+	_, err := u.Authenticate()
+	if err != nil {
+		log.Printf("Failed to authenticate with %v, %v, %v: %v", goodUaaHost, goodUid, goodPass, err)
 		t.Fail()
 	}
 }
 
-func TestUaaClient_Authenticate(t *testing.T) {
-	u := NewUaaClient(uaaHost, uid, pass)
+func TestUaaClient_AuthenticateBadHost(t *testing.T) {
+	u := NewUaaClient(badUaaHost, goodUid, goodPass)
 	_, err := u.Authenticate()
-	if err != nil {
-		log.Printf("Failed to authenticate with %v, %v, %v: %v", uaaHost, uid, pass, err)
+	if err == nil {
+		log.Print("Bad host should fail")
 		t.Fail()
 	}
+	log.Printf("Err: %v", err)
+}
+
+func TestUaaClient_AuthenticateBadUid(t *testing.T) {
+	u := NewUaaClient(goodUaaHost, badUid, goodPass)
+	_, err := u.Authenticate()
+	if err == nil {
+		log.Print("Bad uid should fail")
+		t.Fail()
+	}
+	log.Printf("Err: %v", err)
+}
+
+func TestUaaClient_AuthenticateBadPass(t *testing.T) {
+	u := NewUaaClient(goodUaaHost, goodUid, badPass)
+	_, err := u.Authenticate()
+	if err == nil {
+		log.Print("Bad pass should fail")
+		t.Fail()
+	}
+	log.Printf("Err: %v", err)
 }
